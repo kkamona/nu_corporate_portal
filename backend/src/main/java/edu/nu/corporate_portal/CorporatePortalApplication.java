8b42a95790d8a85136098d8f2ee781ecdc369bcf
Your Name <you@example.com>
2025-04-12 18:09:03 +0500

    asd

--- backend/src/main/java/edu/nu/corporate_portal/CorporatePortalApplication.java
+++ backend/src/main/java/edu/nu/corporate_portal/CorporatePortalApplication.java
@@ -1,28 +1,11 @@
 package edu.nu.corporate_portal;
 
-import edu.nu.corporate_portal.services.*;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CorporatePortalApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CorporatePortalApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner initializeAdmin(UserService userService,
-                                             UserRightsService userRightsService,
-                                             UserProfileService userProfileService,
-                                             PhonebookService phonebookService,
-                                             NuCalendarService nuCalendarService,
-                                             ContentService contentService,
-                                             BirthdayWidgetService birthdayWidgetService) {
-        return args -> {
-
-        };
     }
 }
--- backend/src/main/java/edu/nu/corporate_portal/configs/SecurityConfig.java
+++ backend/src/main/java/edu/nu/corporate_portal/configs/SecurityConfig.java
@@ -1,43 +1,27 @@
-package edu.nu.corporate_portal.config;
+package edu.nu.corporate_portal.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
-import java.security.Key;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/v3/api-docs/**",
-                                "/swagger-ui.html",
-                                "/swagger-ui/**",
-                                "/api/auth/**"
-                        ).permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwt -> jwt.decoder(jwtDecoder()))
-                );
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 
         return http.build();
-    }
-
-    @Bean
-    public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withJwkSetUri("https://login.microsoftonline.com/21fe413c-53ab-42a9-8f23-64725363c212/discovery/v2.0/keys").build();
     }
 }
